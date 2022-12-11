b66a80a7674aa06cbcb29c1d2f1dab0e7efcf0e1
Khanh Pham <khanhpham1091@gmail.com>
2022-12-12 06:45:39 +0700

    Update script

--- configure_kubectl.sh
+++ configure_kubectl.sh
@@ -7,6 +7,7 @@ ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=accept-new ubuntu@$CONTROL_PLA
 [ "$?" -ne 0 ] && echo "WARN: Unable to SSH to $CONTROL_PLANE_IP with key-based authentication from this machine. Skipping." && exit 0
 ssh ubuntu@$CONTROL_PLANE_IP mkdir -p /home/ubuntu/.kube
 ssh ubuntu@$CONTROL_PLANE_IP sudo cp -rf /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
+ssh ubuntu@$CONTROL_PLANE_IP sudo chown ubuntu /home/ubuntu/.kube/config
 mkdir -p ~/.kube
 scp -r ubuntu@$CONTROL_PLANE_IP:/home/ubuntu/.kube/config ~/.kube/.config >/dev/null
 sed "s/127.0.0.1/$CONTROL_PLANE_IP/g" ~/.kube/.config > ~/.kube/config
